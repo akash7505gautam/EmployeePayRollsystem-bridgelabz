d644e5ce53d84b332196b0cd3f327f2784d9d6c0
ambar gautam <akashgautam00234@gmail.com>
2026-02-18 14:24:55 +0530

    day 2 code

--- modules/fileHandler.js
+++ modules/fileHandler.js
@@ -1,15 +1,12 @@
 const fs=require('fs');
-function readFile() 
-{
-    fs.readFile('modules/employees.json',"utf-8",(err,data)=>{
-        if(err)
-        {
-            console.log(err);
-        }
-        else
-        {
-            console.log(data)
-        }
-    })
+function readfile(){
+     fs.readFile('modules/employees.json',"utf-8",(err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(data);
+    }
+})
 }
-module.exports=readFile;
+module.exports=readfile;
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express=require('express');
-const readFile=require('./modules/fileHandler');
-const app =express();
+const fs=require('fs');
+const readfile = require('./modules/fileHandler');
+const app=express();
+app.use(express.static('public'));
+app.set("view engine","ejs");
 app.get('/',(req,res)=>{
-    console.log(readFile());
+    fs.readFile('./modules/employees.json','utf-8',(err,data)=>{
+        if(err){
+            console.log(err);
+        }
+        const employees=JSON.parse(data);
+        res.render('index',{employees});
+    })
 })
 const port=3000;
-app.listen(port,()=>
-{
-    console.log(`server running on ${port}`);
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`);
 })
